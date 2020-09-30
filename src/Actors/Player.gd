4a95193954ea5fd51390961df0fb17111676994d
Diego <diego.villalon@gmail.com>
2020-09-30 02:52:03 +0200

    Tobor Animation States

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,87 +3,86 @@ extends Actor
 export var stomp_impulse = 1000.0
 
 # Da inercia al movimiento (by Diego)
-export var acceleration: = 0.05
-
+export var terrainAcceleration = {
+	"air" : 0.05,
+	"normal" : 0.25,
+	"grass" : 0.25,
+	"stone" : 0.1,
+	"ice": 0.02
+	}
+var acceleration: = 0.25
 var terrain = ""
 
+var state_machine
+var player
+
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
+	player = $player
+
 # Hace saltar al Player cuando un AREA2D entra en la zona especificada
-func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+func _on_EnemyDetector_area_entered(_area: Area2D) -> void:
 	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
 	
 # Mata al Player cuando un BODY entra en la zona especificada
-func _on_EnemyDetector_body_entered(body):
+func _on_EnemyDetector_body_entered(_body):
 	# Mata al player
 	die()
 
-func _on_GroundDetector_body_entered(body: Node) -> void:
-	pass
-"""
-	# Confirm the colliding body is a TileMap
-	print_debug(body)
-	if body is TileMap:
-		print_debug("BODY: "+str(body))
-		# Find the character's position in tile coordinates
-		var tile_pos = body.world_to_map(position)
-		# Find the colliding tile position
-		tile_pos += (Vector2.DOWN)
-		print_debug("Position:" + str(position) + "/ Tile Position: "+ str(tile_pos))
-		# Get the tile id
-		#var tile_id = body.get_cellv(Vector2(0,16))
-		var tile_id = body.get_cellv(tile_pos)
-		print_debug("Tile"+str(tile_id))
-		terrain = body.tile_set.tile_get_name(tile_id)
-		print_debug(terrain)
-	"""
-
 # Esta funcion se ejecuta y ejecuta la misma funcion del padre una vez por physic frame....
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 		
 		
 	# Variable para hacer el salto mas corto si se suelta el boton...
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	#acceleration = set_acceleration(terrain)
+	# Define la aceleracion en base al terreno
+	acceleration = set_acceleration()
 	# Calcula la velocidad de movimiento del KinematicBody2D y la asigna a la variable _velocity
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	
 	# Hace moverse el "KinematicBody2D" con lógica de plataformas (colision con plataformas) usando la _velocity calculada anteriormente
 		# IMPORTANTE: Se redefine la "_velocity" anulando la gravedad si esta sobre una plataforma...
-		# Se podria usar solo el metodo move_and_slide, pero al llegar al fin de una plataforma caeria brucamente.
+		# Se podria usar solo el metodo move_and_slide, pero al llegar al fin de una plataforma caeria bruscamente.
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-	acceleration = set_acceleration()
+
 
 
 func get_direction() -> Vector2:
-	return Vector2(	
+	var dir = Vector2(	
 		# La dirección en X será 1 si aprieta derecha y -1 si aprieta izquierda
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 		)
+	
+	if dir.x > 0: player.scale.x = -1
+	elif dir.x < 0: player.scale.x = 1
+
+	if is_on_floor():
+		if dir.x == 0: state_machine.travel("idle")
+		else: state_machine.travel("run")
+	
+	else: state_machine.travel("run")
+
+	return dir
 
 func set_acceleration():
 	if is_on_floor():
-		
 		var collision = get_slide_collision(0)
 		if collision.collider is TileMap:
 			# Find the character's position in tile coordinates
 			var tile_pos = collision.collider.world_to_map(position)
 			# Find the colliding tile position
 			#tile_pos -= collision.normal
-			#print_debug(tile_pos)
 			var tile_id = collision.collider.get_cellv(tile_pos) # Get the tile id
 			if not tile_id == TileMap.INVALID_CELL:
 				terrain = collision.collider.tile_set.tile_get_name(tile_id)
-			#print_debug(tile_id)
-			#print_debug(terrain)
-	
-	else: return 0.05 # Acceleration on air
-	if terrain == "grass": return 0.5 # Acceleration on grass
-	elif terrain == "stone": return 0.1 # Acceleration on stone
-	elif terrain == "ice": return 0.02 # Acceleration on ice
-	else:
-		return 0.5
-		print_debug(terrain)
+				# print_debug(terrain)
+				if terrain in terrainAcceleration:
+					return terrainAcceleration[terrain] # Assegna acceleration in base al tipo di tile
+				else: return terrainAcceleration["normal"]
+	else: return terrainAcceleration["air"] # Acceleration on air
+	return acceleration
 
 	
 func calculate_move_velocity(
