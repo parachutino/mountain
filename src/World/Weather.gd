f7ae27633ec8ba2bfc2b3eb3bcf4814c03e4d917
Diego <diego.villalon@gmail.com>
2020-10-03 02:57:52 +0200

    Inicio Weather Control

--- src/World/Weather.gd
+++ src/World/Weather.gd
@@ -1,18 +1,28 @@
 extends Node2D
 
-export (String, 'sun', 'rain', 'snow') var weatherType = 'sun'
+export (String, 'clear', 'rain', 'snow') var weatherType = 'sun'
 export (float, -1, 1) var wind = 0
 export (float, 0, 1) var size = 0.3
 export (int, 100, 1000) var amount = 250
+export (float, 0, 1) var light = 1
+
+export var playerNode: NodePath = "../Player"
+export var followNode: NodePath = "../Player"
+
+var nightColor: Color = Color.white # color SUBTRACTED to 
+
+var snow_darkness = 0.2
+var rain_darkness = 0.2
 
 onready var snow = $Snow
 onready var rain = $Rain
+onready var darkness = $Darkness
 
 # Defines Player to set wind behaviour
-onready var player: Node2D = get_node("../Player")
+onready var player: Node2D = get_node(playerNode)
 
 # Emiter folows position of this node.
-onready var follow: Node2D = get_node("../Player")
+onready var follow: Node2D = get_node(followNode)
 
 
 # Declare member variables here. Examples:
@@ -22,13 +32,16 @@ onready var follow: Node2D = get_node("../Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	change_weather() 
+	change_weather()
+	darkness()
 	position = get_viewport_transform().get_origin() + Vector2(get_viewport_rect().size.x / 2, 0) # Initially positions the emiter in the top center of the screen
 	snow.process_material.emission_box_extents.x = get_viewport_rect().size.x * 2 # Sets emiter width to N times the screen size
 	rain.process_material.emission_box_extents.x = get_viewport_rect().size.x * 2 # Sets emiter width to N times the screen size
 	
 func _physics_process(_delta: float) -> void:
-	if follow: position = follow.position + Vector2(0, -get_viewport_rect().size.y) # Follows the position of node in "follow"
+	if follow:
+		position = follow.position + Vector2(0, -get_viewport_rect().size.y) # Follows the position of node in "follow"
+		darkness()
 	
 func change_weather():
 	
@@ -43,10 +56,13 @@ func change_weather():
 		# snow.process_material.set("anim_offset", size) # Alternative way to set property...
 	
 		# SNOW WIND SETTINGS
-		snow.speed_scale = 0.5 + wind / 2
+		snow.speed_scale = 0.5 + abs(wind) / 2
 		snow.process_material.direction.x = wind
 		snow.process_material.gravity.x = 100 * wind
 		snow.process_material.initial_velocity = 100 + 400 * abs(wind)	
+		
+		# DARKEN DAY
+		darkness.color = nightColor.darkened(light-0.2)
 
 	else: snow.emitting = false
 	
@@ -59,15 +75,28 @@ func change_weather():
 		# snow.process_material.set("anim_offset", size) # Alternative way to set property...
 	
 		# RAIN WIND SETTINGS
-		rain.speed_scale = 0.5 + wind / 2
+		rain.speed_scale = 0.5 + abs(wind) / 2
 		rain.process_material.direction.x = wind # / 2
 		rain.process_material.gravity.x = 100 * wind
 		rain.process_material.initial_velocity = 200 + 400 * abs(wind)	
+		
+		# DARKEN DAY
+		darkness.color = nightColor.darkened(light-rain_darkness)
 
 	else: rain.emitting = false
-
-
 	
+	if weatherType == 'clear':
+		darkness.color = nightColor.darkened(light)
+
+
+
+func darkness():
+	
+	# DARKNESS 4 TIMES VIEWPORT SIZE AND CENTERED... I THINK
+	darkness.rect_size = get_viewport_rect().size * 4
+	darkness.rect_position = get_viewport_rect().position - Vector2(get_viewport_rect().size.x*2, get_viewport_rect().size.y)
+#	darkness.rect_size = get_viewport_rect().size * 2
+#	darkness.rect_position = get_viewport_rect().position - Vector2(get_viewport_rect().size.x, 0)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ src/World/WeatherControl.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+export (String, 'clear', 'rain', 'snow') var weatherType = 'sun'
+export (float, -1, 1) var wind = 0
+export (float, 0, 1) var size = 0.3
+export (int, 100, 1000) var amount = 250
+export (float, 0, 1) var light = 1
+export var weatherNode: NodePath = "../Weather"
+var weather: Node2D
+
+onready var collisionShape2D = $Area2D/CollisionShape2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	weather = get_node(weatherNode)
+	collisionShape2D.position = rect_size / 2
+	collisionShape2D.shape.extents = rect_size/2
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Area2D_area_entered(area: Area2D) -> void:
+	print_debug("CHANGE WEATHER!!")
+	weather.weatherType = weatherType
+	weather.wind = wind
+	weather.size = size
+	weather.amount = amount
+	weather. light = light
+	weather.change_weather()
+	pass # Replace with function body.
+
